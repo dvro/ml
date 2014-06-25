d12dcef44f2ac34fda54f088a4b31c0c8d8d2d5b
Dayvid Victor <victor.dvro@gmail.com>
2014-06-25 16:06:11 -0300

    regular10 working (thanks Mario Henrique Santos da Silva)

--- mlcin/utils/keel.py
+++ mlcin/utils/keel.py
@@ -32,7 +32,7 @@ def load_dataset(string, separator=","):
 
     s = filter(lambda e: e[0] != '@', s)
     s = [v.strip().split(separator) for v in s]
-    X = np.asarray([[float(e) for e in v[:-1]] for v in s])
+    X = np.asarray([[float(e) for e in v[:-1]] for v in s if len(v) > 1])
 
     d = {'positive': 1, 'negative': 0}
     y = np.asarray([d[v[-1].strip()] if v[-1].strip() in d else v[-1].strip()
